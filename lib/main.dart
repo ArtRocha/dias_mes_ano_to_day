66fd80717f22642ebf9c38eeb75773f22ac4ddd3
ARTHUR HENRIQUE ROCHA <arthur.rocha@wds.local>
2022-05-07 10:04:19 -0300

    changes to activity

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
@@ -28,14 +30,36 @@ class _HomeState extends State<Home> {
   final anoController = TextEditingController();
   final mesController = TextEditingController();
   final diaController = TextEditingController();
-
+  final nomeController = TextEditingController();
 
   String _mensagem = "";
 
   int ano = 0;
   int mes = 0;
   int dia = 0;
+  String nome = '';
+  DateTime now = new DateTime.now();
   GlobalKey<FormState>_formKey = GlobalKey<FormState>();
+  
+  _calcIdade(){
+    setState(() {
+       //entrada de dados
+      DateTime date = new DateTime(now.year, now.month, now.day);
+
+      ano = int.parse(anoController.text);
+      mes = int.parse(mesController.text);
+      dia = int.parse(diaController.text);
+      nome = nomeController.text;
+
+      //calculando idade
+      int idade = now.year - int.parse(anoController.text);
+
+      //saida
+      _mensagem = "${nome.toString()}: Você tem ${idade.toString()} anos";
+    });
+   
+      
+  }
 
   _diaMesAnoParaDias(){
     setState(() {
@@ -45,20 +69,21 @@ class _HomeState extends State<Home> {
       dia = int.parse(diaController.text);
 
       //processamento
-      int dias = ano * 360 + mes * 30 + dia;
+      int idade = ano * 360 + mes * 30 + dia;
 
       //saida
-      _mensagem = "você tem ${dias.toString()} de vida";
-      anoController.clear();
-      mesController.clear();
-      diaController.clear();
+      _mensagem = "você tem ${idade.toString()}";
+      
     });
   }
+
+ 
 
   void _limpaCampos(){
     anoController.clear();
     mesController.clear();
     diaController.clear();
+    nomeController.clear();  
     setState(() {
       _mensagem = 'Informe seus dados!';
       _formKey = GlobalKey<FormState>();
@@ -71,14 +96,14 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Ano/Mes/Dia para dias - Fatec Ferraz'),
+        title: Text('Calcular Idade - Fatec Ferraz'),
         backgroundColor: Colors.blue[700],
         centerTitle: true,
         actions: <Widget>[
           IconButton(
             onPressed: (){
               if (_formKey.currentState!.validate()){
-                _diaMesAnoParaDias();
+                _calcIdade();
               }
             },
             icon: Icon(Icons.refresh),
@@ -95,31 +120,44 @@ class _HomeState extends State<Home> {
                 Icon(
                   Icons.date_range,
                   size: 150,
-                  color: Colors.blueAccent,
+                  color: Colors.blue,
                 ),
+                Text('Insira sua Data de Nascimento',
+                style: TextStyle(
+                  fontSize: 30, 
+                  color: Colors.blue,
+                  ),
+                
+                ),
+    
+                construirTextField("Nome", "Qtde", nomeController, "Favor Digitar o Nome"),
+                Divider(),
                 construirTextField("Ano", "Qtde", anoController, "Favor Digitar o ano ou anos"),
                 Divider(),
                 construirTextField("Mês", "Qtde", mesController, "Favor Digitar o mes ou meses"),
                 Divider(),
                 construirTextField("Dia", "Qtde", diaController, "Favor Digitar o dia ou dias"),
+                
+                
                 Padding(
                   padding: EdgeInsets.all(20),
-                  child: FlatButton(
-                    child: Text(
-                      "Ano/Mês/Dia para Dias",
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.blue,
-                      ),
-                    ),
-                    onPressed: () {
+                  child: ElevatedButton.icon(
+                     onPressed: () {
                       if(_formKey.currentState!.validate()){
-                      _diaMesAnoParaDias();
+                      _calcIdade();
 
                       }
                     },
+                     style: ElevatedButton.styleFrom(fixedSize: Size(10,40)),
+                     icon: Icon(Icons.calculate, size:30),
+                     label: Text('Calcular Idade'),
+                     
+                     )
+                    
+                    
+               
                     ),
-                ),
+                
                 Center(child: Text(
                   _mensagem,
                   style: TextStyle(
@@ -160,4 +198,6 @@ class _HomeState extends State<Home> {
     );
 
   }
+  
+  
 }
